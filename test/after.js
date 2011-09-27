f846cd0ba2fbe81d2ab4f8974be8edf3507e182f
Jed Schmidt <tr@nslator.jp>
2011-09-27 18:15:44 +0200

    updated output

--- test/after.js
+++ test/after.js
@@ -1,1 +1,1 @@
-with(n=function(a,b,c,d){for(b in a){for(c=a=0;d=b.charCodeAt(c++);a%=934)a+=c*d;n[(a+360).toString(36)]=b}}){n(this);n(this[fk]);n(this[fk][o3]);this[fk][eq][cr](this[fk][e5]('script'))}
+n=function(a,b,c,d){for(b in a){for(c=a=0;d=b.charCodeAt(c++);a%=934)a+=c*d;n[(a+360).toString(36)]=b}};n(this);n(this[fk]);n(this[fk][o3]);this[fk][eq][cr](this[fk][e5]('script'))
